6b14b6e53239a9c3e78de07c812781ee4dc15c48
“Doreen” <doreenbarber@gmail.com>
2023-02-02 16:08:26 +0000

    Add Project 1 files for Guess the Number game.

---
+++ Projects/Project-1-Guess-My-Number/script.js
@@ -0,0 +1,47 @@
+'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+
+// document.querySelector('.message').textContent = '🎉 Correct Number! 🎉';
+
+// document.querySelector('.score').textContent = 10;
+
+// document.querySelector('.guess').value = 23;
+// document.querySelector('.guess').value;
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+// state variable = part of the state of our application
+let score = 20;
+
+// Check the number entered in the app
+document.querySelector('.check').addEventListener('click', function () {
+	const guess = Number(document.querySelector('.guess').value);
+
+	if (!guess) {
+		document.querySelector('.message').textContent = '⛔️ No number!';
+	} else if (guess === secretNumber) {
+		document.querySelector('.message').textContent = '🎉 Correct Number!';
+	} else if (guess > secretNumber) {
+		if (score > 1) {
+			document.querySelector('.message').textContent = '📈 Too high!';
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = '💩 Sozzo, you lost!';
+			score--;
+			document.querySelector('.score').textContent = 0;
+		}
+	} else if (guess < secretNumber) {
+		if (score > 1) {
+			document.querySelector('.message').textContent = '📉 Too low!';
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = '💩 Sozzo, you lost!';
+			score--;
+			document.querySelector('.score').textContent = 0;
+		}
+	}
+});
