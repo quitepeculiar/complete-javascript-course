89ffa5ac3440729493ced659aca24350458d0fa0
“Doreen” <doreenbarber@gmail.com>
2023-02-02 17:10:22 +0000

    Enable Again button to reset app

--- Projects/Project-1-Guess-My-Number/script.js
+++ Projects/Project-1-Guess-My-Number/script.js
@@ -1,16 +1,6 @@
 'use strict';
 
-// console.log(document.querySelector('.message').textContent);
-
-// document.querySelector('.message').textContent = '🎉 Correct Number! 🎉';
-
-// document.querySelector('.score').textContent = 10;
-
-// document.querySelector('.guess').value = 23;
-// document.querySelector('.guess').value;
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 // state variable = part of the state of our application
 let score = 20;
@@ -23,6 +13,7 @@ document.querySelector('.check').addEventListener('click', function () {
 		document.querySelector('.message').textContent = '⛔️ No number!';
 	} else if (guess === secretNumber) {
 		document.querySelector('.message').textContent = '🎉 Correct Number!';
+		document.querySelector('.number').textContent = secretNumber;
 		document.querySelector('body').style.backgroundColor = '#60b347';
 		document.querySelector('.number').style.width = '30rem';
 	} else if (guess > secretNumber) {
@@ -46,4 +37,18 @@ document.querySelector('.check').addEventListener('click', function () {
 			document.querySelector('.score').textContent = 0;
 		}
 	}
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+	secretNumber = Math.trunc(Math.random() * 20) + 1;
+	score = 20;
+
+	document.querySelector('.score').textContent = score;
+	document.querySelector('.message').textContent = 'Start guessing...';
+	document.querySelector('.guess').value = '';
+	document.querySelector('.number').textContent = '?';
+
+	// Restore original background colour and number box width
+	document.querySelector('body').style.backgroundColor = '#222222';
+	document.querySelector('.number').style.width = '15rem';
 });
