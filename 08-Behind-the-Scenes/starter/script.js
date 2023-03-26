1727b4c3acd7018c500efad4070aa36d65df889a
“Doreen” <doreenbarber@gmail.com>
2023-03-26 13:13:08 +0100

    Update from Section 8

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+// From video 93
+function calcAge(birthYear) {
+  const age = 2093 - birthYear;
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}.`;
+    console.log(output);
+
+    if (birthYear >= 1997 && birthYear <= 2012) {
+      const str = `Oh, and you are a Zoomer, ${firstName}!`;
+      console.log(str);
+    }
+  }
+
+  printAge();
+
+  return age;
+}
+
+const firstName = 'Foxx';
+calcAge(2010);
