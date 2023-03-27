749e344a3365f7f3f3575844115e2721633c3659
“Doreen” <doreenbarber@gmail.com>
2023-03-27 15:17:22 +0100

    Complete section 8, start section 9

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -11,7 +11,6 @@ const restaurant = {
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-
   openingHours: {
     thu: {
       open: 12,
@@ -26,4 +25,79 @@ const restaurant = {
       close: 24,
     },
   },
+
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
+    console.log(
+      `Order received: ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
+    );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta dish from Classico Italiano, with ${ing1}, ${ing2}, ${ing3}`
+    );
+  },
 };
+
+restaurant.orderDelivery({
+  time: '22:30',
+  address: '4 Burma Road',
+  mainIndex: 2,
+  starterIndex: 2,
+});
+
+restaurant.orderDelivery({
+  address: '81 Tipo Pils Ave',
+  mainIndex: 2,
+});
+
+const {
+  name: restaurantName,
+  openingHours: hours,
+  categories: tags,
+} = restaurant;
+console.log(restaurantName, hours, tags);
+
+const { menu = [], starterMenu: starters = [] } = restaurant;
+console.log(menu, starters);
+
+// new array with the spread operator
+const newMenu = [...restaurant.mainMenu, 'Gnocci'];
+
+// Copy array
+const mainMenuCopy = [...restaurant.mainMenu];
+
+// Join 2 arrays
+const restaurantFullMenu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+console.log(`This is the full menu: ${restaurantFullMenu}`);
+
+// Nested objects - open and close
+const {
+  fri: { open: friOpen, close: friClosed },
+} = hours;
+console.log(friOpen, friClosed);
+
+// const ingredients = [
+//   prompt("Let's make pasta! Ingredient 1?"),
+//   prompt('Ingredient 2?'),
+//   prompt('Ingredient 3?'),
+// ];
+
+// restaurant.orderPasta(...ingredients);
+
+// Objects
+const newRestaurantFranchise = { ...restaurant, location: 'Tokyo', branch: 1 };
+console.log(newRestaurantFranchise);
+
+// Making object copies
+const restaurantCopy = { ...restaurant };
+restaurantCopy.name = 'Ristorante Roma';
